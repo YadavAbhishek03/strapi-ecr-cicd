930f2692d18cbd604e4169bde418b0be8ad23d7a
YadavAbhishek03 <suryadav1410@gmail.com>
2025-04-27 09:29:38 +0000

    updated terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "abhi-terraform-state-bucket"  # Replace with your bucket name
-    key    = "terraform/terraform.tfstate"  # Specify the path to your state file
+    key    = "terraform/terraform.tfstate"  # Specify the path to your state file where your .tfstate file
     region = "us-east-1"
     encrypt = true  # Enables encryption for state file
   }
