1306f687bf8710d06084815fc2890343c4e754c3
YadavAbhishek03 <suryadav1410@gmail.com>
2025-04-28 08:33:55 +0000

    updated terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,6 @@ provider "aws" {
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 
   tags = {
     Name = "abhi-strapi-vpc"
