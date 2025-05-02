3c1791f6791a9aa286d7f68faf62feea6df4d621
YadavAbhishek03 <suryadav1410@gmail.com>
2025-05-02 09:30:49 +0000

    update workflows file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@ output "strapi_url" {
   value = "http://${aws_lb.alb.dns_name}/admin"
   description = "Public URL to access Strapi"
 }
+
+output "blue_target_group_arn" {
+  value = aws_lb_target_group.blue.arn
+}
+
+output "green_target_group_arn" {
+  value = aws_lb_target_group.green.arn
+}
